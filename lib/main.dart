0f61de7d8157760ab1081ba449c019a462727189
vyPal <kubik.palacky@gmail.com>
2023-09-25 16:19:22 +0200

    GestureDetector sort of working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -55,18 +57,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Widget> _buttons = [];
 
   @override
   Widget build(BuildContext context) {
@@ -86,40 +77,39 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: GestureDetector(
+          onTapDown: (details) {
+            setState(() {
+              _buttons.add(
+                Positioned(
+                  left: details.localPosition.dx - 13,
+                  top: details.localPosition.dy - 13,
+                  child: Image.asset(
+                    "assets/x1multiplier.png",
+                    width: 26,
+                  ),
+                ),
+              );
+            });
+            Timer(Duration(seconds: 2), () {
+              setState(() {
+                _buttons.removeAt(0);
+              });
+            });
+          },
+          child: Stack(
+            children: [
+              Center(
+                child: Image.asset("assets/balloon.png"),
+              ),
+              ..._buttons,
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
